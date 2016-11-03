9da96fb83165a2399643c1f69355cb66faf77c6a
Albin Paul <albinpaul.ks@gmail.com>
2016-11-03 18:58:13 +0530

    barebones

---
+++ application/views/front_page.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Student Website</title>
+</head>
+<body>
+	<div id="container">
+		<h1>We are here to help you</h1>
+
+		<div id="body">
+
+
+		</div>
+	</div>
+</body>
+</html>
